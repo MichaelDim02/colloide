4f9008a32fea9506ff4162159109a917ee664345
Nic <fristurt@hotmail.com>
2017-11-08 02:46:50 +0100

    tor passthrough

--- colloide.py
+++ colloide.py
@@ -5,7 +5,25 @@
 import sys
 import argparse
 import os
-from urllib2 import Request, urlopen, URLError, HTTPError
+import socks
+import socket
+try: # For Python 3.0 and later
+	from urllib.request import Request, urlopen, build_opener, install_opener, ProxyHandler
+	from urllib.error import URLError, HTTPError
+except ImportError: # Fall back to Python 2's urllib2
+	from urllib2 import Request, urlopen, ProxyHandler, URLError, HTTPError
+
+# STEM module for signaling tor network service
+from stem import Signal
+from stem.control import Controller
+
+DEBUG_OUTPUT_DIR = "output/"
+
+TOR_DEFAULT_CONTROLLER_PROXY_PORT = 9051
+TOR_DEFAULT_PROXY_IP = "127.0.0.1"
+TOR_DEFAULT_PROXY_PORT = 9050
+
+controller = Controller.from_port(port = TOR_DEFAULT_CONTROLLER_PROXY_PORT)
 
 def banner():
 	print("_________        .__  .__         .__    .___     ")
@@ -21,7 +39,7 @@ def opts():
 	print("    -u, --URL        The URL to the website")
 	print("    -p, --pages      Path to the wordlist with the page names / links")
 	print("    -l, --legals     License & legal disclaimer")
-    print("    -t, --torenable  Enable tor proxy switching (!!! REQUIRES CONTROLLER PORT OPEN !!!)\n\n")
+	print("    -t, --torenable  Enable tor proxy switching (!!! REQUIRES CONTROLLER PORT OPEN !!!)\n\n")
 def legals():
 	#License
 	print("Colloide version 0.5 is free software. It can be re-distributed ")
@@ -51,21 +69,27 @@ def wolf():
 	print("_______| /|.'       '.l \\\_")
 	print("       || ||             '-'")
 	print("       '-''-'\n")
-def check_names(infile):
+def check_names(infile, torEnabled=0):
 	if os.path.exists(infile):
 		banner()
 		wolf()
-		findAdmin()
+		findAdmin(torEnabled)
 	else:
 		banner()
 		opts()
-		print
-        ("Invalid path to the wordlis. File could not be found.")
+		print("Invalid path to the wordlis. File could not be found.")
 
-def findAdmin():
+def findAdmin(torEnabled=0):
 	f = open(links,"r");
 	print("[!] Report bugs: anivsante2@gmail.com \n") # https://github.com/MichaelDim02/colloide/issues instead?
+
+	if torIsEnabled:
+		socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, TOR_DEFAULT_PROXY_IP, TOR_DEFAULT_PROXY_PORT)
+		socket.socket = socks.socksocket
+		print("You have enabled Tor proxy passthrough. Please be aware that this might significantly slow down the scan.")
+
 	while True:
+		generateNewTorIP()
 		sub_link = f.readline()
 		if not sub_link:
 			break
@@ -81,19 +105,36 @@ def findAdmin():
 		else:
 			print("[+] Link Found -> ",req_link)
 
+def generateNewTorIP(password="el_passwordo", controllerPort=TOR_DEFAULT_CONTROLLER_PROXY_PORT):
+    controller.authenticate(password=password)
+    controller.signal(Signal.NEWNYM)
 
+def log(name, data):
+	if not os.path.exists(DEBUG_OUTPUT_DIR):
+		os.makedirs(DEBUG_OUTPUT_DIR)
+	logFile = open(DEBUG_OUTPUT_DIR + name.replace("/", ""), "w")
+
+	req = Request('http://httpbin.org/ip')
+	try:
+		response = urlopen(req)
+	except HTTPError as e:
+		print(e)
+	except URLError as e:
+		print(e)
+
+	logFile.write(str(response.read(200)))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--URL", help="The URL to the website")
 parser.add_argument("-p", "--pages", help="Path to the wordlist with the page names / links")
 parser.add_argument("-l", "--legals", action='store_true', help="License & legal disclaimer")
-parser.add_argument("-t", "--torenable", help="Enable proxying through TOR to anonymize traffic (requires TOR to be running)")
+parser.add_argument("-t", "--torenable", help="Enable proxying through TOR to anonymize traffic (requires TOR to be running and controller configured (default port 9051)) !!! WILL INCREASE RUN TIME DRAMATICALLY !!!")
 args = parser.parse_args()
 links = args.pages
 URL = args.URL
-torEnable = args.torenable
+torIsEnabled = args.torenable
 if args.URL and args.pages:
-	check_names(links)
+	check_names(links, torEnabled=args.torenable)
 elif args.legals:
 	banner()
 	legals()
